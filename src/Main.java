e2caf464220d55d4901efcee512e320aaadf42ee
davutcagri <71286320+davutcagri@users.noreply.github.com>
2023-07-04 15:42:31 +0300

    davutcagri

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// 5 elemanlı bır string array tanımlanacaktır.
+// Arrayin uzunluğu, ilk ve son elemanları ekrana yazdırılacaktır.
+public class Main {
+    public static void main(String[] args) {
+        String [] sehirler = {"Bakü", "Istanbul", "Paris", "Roma"};
+
+        System.out.println(sehirler[0]); // İlk eleman
+        System.out.println(sehirler[3]); // Son eleman
+        System.out.println(sehirler.length); // Array Uzunluğu
+    }
+}
